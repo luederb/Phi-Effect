7cb859813ad3822c7727d3d147d3b748512f531b
Luederb <lueder.budde@gmail.com>
2024-04-10 18:17:17 +0100

    Rearrange FriendRequests and Network components

--- frontend/src/components/Network/FriendRequests/FriendRequests.tsx
+++ frontend/src/components/Network/FriendRequests/FriendRequests.tsx
@@ -4,20 +4,24 @@ import {Logger} from "../../../Logger/Logger.tsx";
 import {FriendRequest} from "../../../Types/FriendRequest.ts";
 import UserCard from "../UserCard/UserCard.tsx";
 import {useState} from "react";
+import {User} from "../../../Types/User.ts";
 
 type FriendRequestsProps = {
     receivedFriendRequests: FriendRequest[];
     handleSetReceivedFriendRequests: (value: FriendRequest[]) => void;
     sentFriendRequests: FriendRequest[];
     handleSetSentFriendRequests: (value: FriendRequest[]) => void;
+    friends: User[];
 }
 export default function FriendRequests({
                                            receivedFriendRequests,
                                            handleSetReceivedFriendRequests,
                                            sentFriendRequests,
-                                           handleSetSentFriendRequests
+                                           handleSetSentFriendRequests,
+                                           friends
                                        }: Readonly<FriendRequestsProps>) {
     const [expandedUserId, setExpandedUserId] = useState<string | null>(null);
+
     function onUserCardClick(id: string) {
         if (expandedUserId === id) {
             setExpandedUserId(null);
@@ -25,6 +29,7 @@ export default function FriendRequests({
             setExpandedUserId(id);
         }
     }
+
     function acceptFriendRequest(friendId: string, friendRequestId: string) {
         axios.put(`/api/users/${friendId}/friendRequests/${friendRequestId}/accept`)
             .then(response => {
@@ -50,66 +55,72 @@ export default function FriendRequests({
 
     return (
         <div className="friend-request-list">
-            {receivedFriendRequests && receivedFriendRequests.length > 0 &&                <>
-                    <h3>Received Friend Requests</h3>
-                    <ul className="friends-requests-list">
-                        {receivedFriendRequests.map(receivedFriendRequest => (
-                            <li key={receivedFriendRequest.id} className="friend-request">
-                                <button onClick={() => onUserCardClick(receivedFriendRequest.sender.id)}>
+            {receivedFriendRequests && receivedFriendRequests.length > 0 && <>
+                <h3>Received Friend Requests</h3>
+                <ul className="friends-requests-list">
+                    {receivedFriendRequests.map(receivedFriendRequest => (
+                        <li key={receivedFriendRequest.id} className="friend-request">
+                            <button onClick={() => onUserCardClick(receivedFriendRequest.sender.id)}>
                                 <UserCard receivedFriendRequest={receivedFriendRequest}
-                                    user={receivedFriendRequest.sender}
-                                          isExpanded={receivedFriendRequest.sender.id === expandedUserId}/>
-                                </button>
-                                {receivedFriendRequest.status === "pending" &&
-                                    <>
-                                        <button className="classic-button"
-                                                onClick={() => rejectFriendRequest(receivedFriendRequest.receiver.id, receivedFriendRequest.id)}>reject
-                                        </button>
-                                        <button className="classic-button"
-                                                onClick={() => acceptFriendRequest(receivedFriendRequest.receiver.id, receivedFriendRequest.id)}>accept
-                                        </button>
-                                    </>
-                                }
-                                {receivedFriendRequest.status === "accepted" &&
-                                    <p>Accepted</p>
-                                }
-                                {receivedFriendRequest.status === "rejected" &&
-                                    <p>Rejected</p>
-                                }
-                            </li>
-                        ))}
-                    </ul>
-                </>
+                                          user={receivedFriendRequest.sender}
+                                          isExpanded={receivedFriendRequest.sender.id === expandedUserId}
+                                          isFriend={
+                                              friends.some(friend => friend.id === receivedFriendRequest.sender.id)
+                                          }/>
+                            </button>
+                            {receivedFriendRequest.status === "pending" &&
+                                <>
+                                    <button className="classic-button"
+                                            onClick={() => rejectFriendRequest(receivedFriendRequest.receiver.id, receivedFriendRequest.id)}>reject
+                                    </button>
+                                    <button className="classic-button"
+                                            onClick={() => acceptFriendRequest(receivedFriendRequest.receiver.id, receivedFriendRequest.id)}>accept
+                                    </button>
+                                </>
+                            }
+                            {receivedFriendRequest.status === "accepted" &&
+                                <p>Accepted</p>
+                            }
+                            {receivedFriendRequest.status === "rejected" &&
+                                <p>Rejected</p>
+                            }
+                        </li>
+                    ))}
+                </ul>
+            </>
             }
-            {sentFriendRequests && sentFriendRequests.length > 0 &&                <>
-                    <h3>Sent Friend Requests</h3>
-                    <ul className="friends-requests-list">
-                        {sentFriendRequests.map(sentFriendRequest => (
-                            <li key={sentFriendRequest.id} className="friend-request">
-                                <button onClick={() => onUserCardClick(sentFriendRequest.sender.id)}>
-                                    <UserCard sentFriendRequest={sentFriendRequest}
-                                              user={sentFriendRequest.receiver}
-                                              isExpanded={sentFriendRequest.sender.id === expandedUserId}/>
-                                </button>
-                                {sentFriendRequest.status === "pending" &&
-                                    <>
-                                        <button className="classic-button"
-                                                onClick={() => acceptFriendRequest(sentFriendRequest.receiver.id, sentFriendRequest.id)}>accept
-                                        </button>
-                                        <button className="classic-button"
-                                                onClick={() => rejectFriendRequest(sentFriendRequest.receiver.id, sentFriendRequest.id)}>reject
-                                        </button>
-                                    </>
-                                }
-                                {sentFriendRequest.status === "accepted" &&
-                                    <p>Accepted</p>
-                                }
-                                {sentFriendRequest.status === "rejected" &&
-                                    <p>Rejected</p>
-                                }
-                            </li>
-                        ))}
-                    </ul>
+            {sentFriendRequests && sentFriendRequests.length > 0 && <>
+                <h3>Sent Friend Requests</h3>
+                <ul className="friends-requests-list">
+                    {sentFriendRequests.map(sentFriendRequest => (
+                        <li key={sentFriendRequest.id} className="friend-request">
+                            <button onClick={() => onUserCardClick(sentFriendRequest.sender.id)}>
+                                <UserCard sentFriendRequest={sentFriendRequest}
+                                          user={sentFriendRequest.receiver}
+                                          isExpanded={sentFriendRequest.sender.id === expandedUserId}
+                                          isFriend={
+                                              friends.some(friend => friend.id === sentFriendRequest.receiver.id)
+                                          }/>
+                            </button>
+                            {sentFriendRequest.status === "pending" &&
+                                <>
+                                    <button className="classic-button"
+                                            onClick={() => acceptFriendRequest(sentFriendRequest.receiver.id, sentFriendRequest.id)}>accept
+                                    </button>
+                                    <button className="classic-button"
+                                            onClick={() => rejectFriendRequest(sentFriendRequest.receiver.id, sentFriendRequest.id)}>reject
+                                    </button>
+                                </>
+                            }
+                            {sentFriendRequest.status === "accepted" &&
+                                <p>Accepted</p>
+                            }
+                            {sentFriendRequest.status === "rejected" &&
+                                <p>Rejected</p>
+                            }
+                        </li>
+                    ))}
+                </ul>
             </>
             }
         </div>
--- frontend/src/components/Network/UserCard/UserCard.tsx
+++ frontend/src/components/Network/UserCard/UserCard.tsx
@@ -1,10 +1,11 @@
 import "./UserCard.css";
 import {User} from "../../../Types/User.ts";
-import Icon from "../../../assets/Icons/Icons.tsx";
 import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import {Logger} from "../../../Logger/Logger.tsx";
 import {FriendRequest} from "../../../Types/FriendRequest.ts";
+import StandardUserInformation from "./StandardUserInformation/StandardUserInformation.tsx";
+import UserCardHeader from "./UserCardHeader/UserCardHeader.tsx";
 
 type UserCardProps = {
     user: User;
@@ -54,45 +55,19 @@ export default function UserCard({
     return (
         <div className="user-card">
             <div className={`expandable-user-card-content ${isExpanded ? "show" : ""}`}>
-                <div className="user-card-header">
-                    {isExpanded ?
-                        <Icon variant="lessArrow" size={20} backgroundColor="var(--standardFondColor)"
-                              strokeColor="var(--standardFondColor)" strokeWidth={1}/>
-                        :
-                        <Icon variant="moreArrow" size={20} backgroundColor="var(--standardFondColor)"
-                              strokeColor="var(--standardFondColor)" strokeWidth={1}/>
-                    }
-                    <p>{user.name}</p>
-                    <img className="user-picture" src={user.picture} alt="profile"/>
-                </div>
+                <UserCardHeader user={user} isExpanded={isExpanded}/>
+
                 {!isFriend && (receivedFriendRequest && receivedFriendRequest.receiver.id === user.id) &&
                     <>
                         <p>Received at: </p>
                         <p>{new Date(receivedFriendRequest.timestamp).toLocaleDateString()}</p>
                     </>
                 }
-                <div className="user-card-line">
-                    <p>Email: </p>
-                    <p>{user.email}</p>
-                </div>
-                {user.phone !== 0 &&
-                    <div className="user-card-line">
-                        <p>Phone: </p>
-                        <p className="phone-number">{`0${user.phone?.toString().slice(0, 3)}  -  ${user.phone?.toString().slice(3, 5)} ${user.phone?.toString().slice(5, 7)} ${user.phone?.toString().slice(7, 9)} ${user.phone?.toString().slice(9)}`}</p>
-                    </div>}
-                {user.bio !== "" &&
-                    <div className="user-card-line">
-                        <p>Bio: </p>
-                        <p>{user.bio}</p>
-                    </div>}
-                {user.favoriteProjects?.entries() &&
-                    <div className="user-card-line">
-                        <p>Favorite Projects: </p>
-                        <p>{user.favoriteProjects}</p>
-                    </div>}
+                <StandardUserInformation user={user}/>
 
                 {isFriend &&
-                    <button className="classic-button" onClick={handleRemoveFriend && (() => handleRemoveFriend(user.id))}>Remove from
+                    <button className="classic-button"
+                            onClick={handleRemoveFriend && (() => handleRemoveFriend(user.id))}>Remove from
                         Friends</button>
                 }
                 {!isFriend && sentFriendRequests.some(request => request.receiver.id === user.id) &&
